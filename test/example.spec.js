626ebcd41d3b3b2fb240b6e15e0322f60ebaad84
Dzmitry Dauhun <51424255+dmdauhun@users.noreply.github.com>
2024-05-15 21:06:37 -0400

    test messg (#3)

---
+++ test/example.spec.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+//import { expect } from 'chai';
+require('colors');
+
+
+
+describe('Math simple 1', () => {
+    it('verify sum', () => {
+        expect(4+4).eq(8)
+    });
+});
+describe('Math simple 2', () => {
+    it('verify sum', () => {
+        expect(4+4).eq(8)
+    });
+});
